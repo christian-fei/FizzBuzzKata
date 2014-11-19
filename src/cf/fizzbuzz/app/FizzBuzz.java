39ef3830b56d9f4eccfefbeb0996ba2883ffa692
Christian Fei <crifei93@gmail.com>
2014-11-19 08:47:59 +0100

    removed dup zero check in FizzBuzz class

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -3,22 +3,31 @@ package cf.fizzbuzz.app;
 public class FizzBuzz {
 
 	public String evaluate(int number) {
-		if( isMultipleOfFifteen(number) ) return "FizzBuzz";
-		if( isMultipleOfFive(number) ) return "Buzz";
-		if( isMultipleOfThree(number) ) return "Fizz";
+		if (isZero(number))
+			return String.valueOf(number);
+		if (isMultipleOfFifteen(number))
+			return "FizzBuzz";
+		if (isMultipleOfFive(number))
+			return "Buzz";
+		if (isMultipleOfThree(number))
+			return "Fizz";
 		return String.valueOf(number);
 	}
 
+	private boolean isZero(int number) {
+		return 0 == number;
+	}
+
 	private boolean isMultipleOfFifteen(int number) {
-		return number%15 == 0 && number>0;
+		return number % 15 == 0;
 	}
 
 	private boolean isMultipleOfFive(int number) {
-		return number%5 == 0 && number>0;
+		return number % 5 == 0;
 	}
 
 	private boolean isMultipleOfThree(int number) {
-		return number%3 == 0 && number>0;
+		return number % 3 == 0;
 	}
 
 }
