3315ee274ea4c34e57b61b1521827f07d38099b6
Christian Fei <crifei93@gmail.com>
2014-11-27 08:49:37 +0100

    fmt src

--- src/cf/fizzbuzz/test/FizzBuzzTest.java
+++ src/cf/fizzbuzz/test/FizzBuzzTest.java
@@ -1,6 +1,6 @@
 package cf.fizzbuzz.test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -11,37 +11,37 @@ public class FizzBuzzTest {
 
 	private FizzBuzz fb;
 
-	
 	@Before
 	public void setUp() throws Exception {
 		fb = new FizzBuzz();
 	}
+
 	@Test
 	public void evaluate_number0_returns0() throws Exception {
-		assertEquals("0",fb.evaluate(0));
+		assertEquals("0", fb.evaluate(0));
 	}
 
 	@Test
 	public void evaluate_number1_returns1() throws Exception {
-		assertEquals("1",fb.evaluate(1));		
+		assertEquals("1", fb.evaluate(1));
 	}
-	
+
 	@Test
 	public void evaluate_multipleOf3_returnsFizz() throws Exception {
-		assertEquals("Fizz",fb.evaluate(6));
-		assertEquals("Fizz",fb.evaluate(9));
+		assertEquals("Fizz", fb.evaluate(6));
+		assertEquals("Fizz", fb.evaluate(9));
 	}
 
 	@Test
 	public void evaluate_multipleOf5_returnsBuzz() throws Exception {
-		assertEquals("Buzz",fb.evaluate(5));
-		assertEquals("Buzz",fb.evaluate(10));
+		assertEquals("Buzz", fb.evaluate(5));
+		assertEquals("Buzz", fb.evaluate(10));
 	}
-	
+
 	@Test
 	public void evaluate_multipleOf15_returnsFizzBuzz() throws Exception {
-		assertEquals("FizzBuzz",fb.evaluate(15));
-		assertEquals("FizzBuzz",fb.evaluate(30));
+		assertEquals("FizzBuzz", fb.evaluate(15));
+		assertEquals("FizzBuzz", fb.evaluate(30));
 	}
-	
+
 }
