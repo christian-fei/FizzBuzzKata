be605b09cb1ec065c606472958bb3b00a5621638
Christian Fei <crifei93@gmail.com>
2014-11-27 22:40:14 +0100

    removed Buzz case from FizzBuzz

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -13,8 +13,6 @@ public class FizzBuzz {
 	public static String getExclamationFor(int number) {
 		if (number%15 == 0 && number > 0)
 			return "FizzBuzz";
-		if (number%5 == 0 && number > 0)
-			return "Buzz";
 		for (Rule rule : rules) {
 			if(rule.canHandle(number))
 				return rule.evaluate(number);
