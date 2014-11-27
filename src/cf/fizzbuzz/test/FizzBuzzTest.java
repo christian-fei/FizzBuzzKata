775264656cc6385f354c8e75296a992f29098c23
Christian Fei <crifei93@gmail.com>
2014-11-27 08:36:12 +0100

    added throws declaration to test

--- src/cf/fizzbuzz/test/FizzBuzzTest.java
+++ src/cf/fizzbuzz/test/FizzBuzzTest.java
@@ -1,6 +1,6 @@
 package cf.fizzbuzz.test;
 
-import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.*;
 
 import org.junit.Test;
 
@@ -9,9 +9,9 @@ import cf.fizzbuzz.app.FizzBuzz;
 public class FizzBuzzTest {
 
 	@Test
-	public void evaluate_number0_returns0() {
+	public void evaluate_number0_returns0() throws Exception {
 		FizzBuzz fb = new FizzBuzz();
 		assertEquals("0",fb.evaluate(0));
 	}
-
+	
 }
