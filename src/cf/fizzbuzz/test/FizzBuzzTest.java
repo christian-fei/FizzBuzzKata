6834b46386412fc12eced8d07506693d50857ca9
Christian Fei <crifei93@gmail.com>
2014-11-27 22:21:22 +0100

    added failing test for simple numbers

--- src/cf/fizzbuzz/test/FizzBuzzTest.java
+++ src/cf/fizzbuzz/test/FizzBuzzTest.java
@@ -9,8 +9,15 @@ import cf.fizzbuzz.app.FizzBuzz;
 public class FizzBuzzTest {
 
 	@Test
-	public void getExclamation_number0_returns0() {
+	public void getExclamationFor_number0_returns0() {
 		assertEquals("0", FizzBuzz.getExclamationFor(0));
 	}
+	
+	@Test
+	public void getExclamationFor_simpleNumbers_returnsNumber() {
+		assertEquals("1", FizzBuzz.getExclamationFor(1));
+	}
+	
+	
 
 }
