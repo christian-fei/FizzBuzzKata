4d9b70fe4dfe13da009e2c34360cb7c351b4a3f0
Christian Fei <crifei93@gmail.com>
2014-11-27 08:38:53 +0100

    ref test, added setUp fucntion

--- src/cf/fizzbuzz/test/FizzBuzzTest.java
+++ src/cf/fizzbuzz/test/FizzBuzzTest.java
@@ -2,21 +2,27 @@ package cf.fizzbuzz.test;
 
 import static org.junit.Assert.*;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import cf.fizzbuzz.app.FizzBuzz;
 
 public class FizzBuzzTest {
 
+	private FizzBuzz fb;
+
+	
+	@Before
+	public void setUp() throws Exception {
+		fb = new FizzBuzz();
+	}
 	@Test
 	public void evaluate_number0_returns0() throws Exception {
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("0",fb.evaluate(0));
 	}
 
 	@Test
 	public void evaluate_number1_returns1() throws Exception {
-		FizzBuzz fb = new FizzBuzz();
 		assertEquals("1",fb.evaluate(1));		
 	}
 	
