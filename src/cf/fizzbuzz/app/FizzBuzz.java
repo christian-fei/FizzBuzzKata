f4253a496d95382e590547490384b74c38d4567e
Christian Fei <crifei93@gmail.com>
2014-11-27 22:32:56 +0100

    handled multiples of 15, had to change order of ifs in FizzBuzz

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -3,12 +3,12 @@ package cf.fizzbuzz.app;
 public class FizzBuzz {
 
 	public static String getExclamationFor(int number) {
-		if (15 == number)
+		if (number%15 == 0 && number > 0)
 			return "FizzBuzz";
-		if (number%3 == 0 && number > 0)
-			return "Fizz";
 		if (number%5 == 0 && number > 0)
 			return "Buzz";
+		if (number%3 == 0 && number > 0)
+			return "Fizz";
 		return Integer.toString(number);
 	}
 
