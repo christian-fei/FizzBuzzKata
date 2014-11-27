4d742702d536713b95a476ced93ecc07f3eb6603
Christian Fei <crifei93@gmail.com>
2014-11-27 08:44:03 +0100

    handled number 0 case to return '0', to handle modulo

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -3,13 +3,19 @@ package cf.fizzbuzz.app;
 public class FizzBuzz {
 
 	public String evaluate(int number) {
+		if (isZero(number))
+			return "0";
 		if (isFizz(number))
 			return "Fizz";
 		return Integer.toString(number);
 	}
 	
+	private boolean isZero(int number) {
+		return number == 0;
+	}
+
 	private boolean isFizz(int number) {
-		return number == 3;
+		return number % 3 == 0;
 	}
 
 }
