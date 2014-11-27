80f82526016f8622c7c0d3eac3c12d39109a1e3d
Christian Fei <crifei93@gmail.com>
2014-11-27 22:25:34 +0100

    made Buzz test pass with simple if

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -4,6 +4,7 @@ public class FizzBuzz {
 
 	public static String getExclamationFor(int number) {
 		if( 3==number ) return "Fizz";
+		if( 5==number ) return "Buzz";
 		return Integer.toString(number);
 	}
 
