20e0691475d3200625c422ab8635e78cce03fada
Christian Fei <crifei93@gmail.com>
2014-11-27 08:37:39 +0100

    added test for number 1

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -3,6 +3,8 @@ package cf.fizzbuzz.app;
 public class FizzBuzz {
 
 	public String evaluate(int number) {
+		if (number == 1)
+			return "1";
 		return "0";
 	}
 
--- src/cf/fizzbuzz/test/FizzBuzzTest.java
+++ src/cf/fizzbuzz/test/FizzBuzzTest.java
@@ -13,5 +13,11 @@ public class FizzBuzzTest {
 		FizzBuzz fb = new FizzBuzz();
 		assertEquals("0",fb.evaluate(0));
 	}
+
+	@Test
+	public void evaluate_number1_returns1() throws Exception {
+		FizzBuzz fb = new FizzBuzz();
+		assertEquals("1",fb.evaluate(1));		
+	}
 	
 }
