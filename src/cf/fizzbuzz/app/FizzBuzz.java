dd3e7397a12089d57e23708b2c5167a0b314643f
Christian Fei <crifei93@gmail.com>
2014-11-27 22:43:27 +0100

    added FizzBuzzRule

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -6,14 +6,13 @@ import java.util.List;
 public class FizzBuzz {
 	
 	private static List<Rule> rules = new ArrayList<Rule>() {{
-		add(new FizzRule());
+		add(new FizzBuzzRule());
 		add(new BuzzRule());
+		add(new FizzRule());
 		add(new SimpleNumberRule());
 	}};
 	
 	public static String getExclamationFor(int number) {
-		if (number%15 == 0 && number > 0)
-			return "FizzBuzz";
 		for (Rule rule : rules) {
 			if(rule.canHandle(number))
 				return rule.evaluate(number);
