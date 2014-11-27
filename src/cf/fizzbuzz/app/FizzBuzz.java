996801ef41178a9643f1f0bd03be0c229ee2ab5c
Christian Fei <crifei93@gmail.com>
2014-11-27 22:23:10 +0100

    refactored FizzBuzz to return Integer toString of simple number

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -3,8 +3,7 @@ package cf.fizzbuzz.app;
 public class FizzBuzz {
 
 	public static String getExclamationFor(int number) {
-		if( 1==number ) return "1";
-		return "0";
+		return Integer.toString(number);
 	}
 
 }
