e41d3129b743dfe1d674255106da4f4435dd4b5b
Christian Fei <crifei93@gmail.com>
2014-11-27 22:30:32 +0100

    handled multiples of three, had to change order of ifs in FizzBuzz

--- src/cf/fizzbuzz/app/FizzBuzz.java
+++ src/cf/fizzbuzz/app/FizzBuzz.java
@@ -3,12 +3,12 @@ package cf.fizzbuzz.app;
 public class FizzBuzz {
 
 	public static String getExclamationFor(int number) {
-		if (3 == number)
-			return "Fizz";
 		if (5 == number)
 			return "Buzz";
 		if (15 == number)
 			return "FizzBuzz";
+		if (number%3 == 0 && number > 0)
+			return "Fizz";
 		return Integer.toString(number);
 	}
 
